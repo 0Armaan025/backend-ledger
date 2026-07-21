69636f8706d15245f41bc87fb11c1285ad80da25
0Armaan025 <armaan33000@gmail.com>
2026-07-21 12:28:03 +0530

    || update -> some changes ||

--- backend-ledger/src/models/user.model.js
+++ backend-ledger/src/models/user.model.js
@@ -1,8 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: [true]
-    }
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      lowercase: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Invalid email address!",
+      ],
+      unique: [true, "Email already exists"],
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [6, "Password should contain at least 6 characters"],
+      select: false, // does not allow to get passwords from queries later
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+
+  return next();
 });
+
+userSchema.methods.comparePassword = async function (password) {};
+
